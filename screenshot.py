7ab9ab8815a64f91aabe98251e9e2ac18babf725
santanupal364 <santanupal364@gmail.com>
2024-02-20 00:34:46 +0530

    code updated to object oriented approach

--- screenshot.py
+++ screenshot.py
@@ -1,14 +1,44 @@
-import time 
+import time
 import pyautogui
+from PIL import Image
 
 
-def screenshot():
-    fileName = int(round(time.time()* 1000))
-    fileName = '{}.png'.format(fileName)
-    # time.sleep(3)
-    img = pyautogui.screenshot('./images/{}'.format(fileName))
-    img.show()
+class Agent():
+
+    def __init__(self) -> None:
+
+        self.filename = self.generateFilename()
+        self.filePath = None
+
+    def generateFilename(self) -> str:
+        fileName = int(round(time.time() * 1000))
+        fileName = '{}.png'.format(fileName)
+        print("Filename successfully generated")
+
+        return fileName
+
+    # Method to take the screenshot
+    def screenshot(self, dirPath) -> None:
+
+        try:
+            pyautogui.screenshot('{}\{}'.format(dirPath, self.filename))
+            self.filePath = '{}\{}'.format(dirPath, self.filename)
+
+            # Display screenshot
+            self.displayImage()
+        except Exception as e:
+            print("Got Error: ", e)
+
+    # Method to display the captured screeenshot
+    def displayImage(self) -> None:
+        img = Image.open(self.filePath)
+        img.show()
+        print("File is shown..")
 
 
-# if __name__ == "__main__":
-#     screenshot()
+# Define top level module
+if __name__ == "__main__":
+    myAgent = Agent()
+
+    myAgent.screenshot(".")
+    time.sleep(3)
